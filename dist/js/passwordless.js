ea43f1054312c2fb739ba6bc3feb7d6b046f23a2
Jesse R Weigel <jesse@jesseweigel.com>
2016-11-29 11:00:51 -0500

    Add more descriptive error text

--- dist/js/passwordless.js
+++ dist/js/passwordless.js
@@ -14,7 +14,7 @@
       var hash = lock.parseHash(window.location.hash);
 
       if (hash && hash.error) {
-        alert('There was an error: ' + hash.error + '\n' + hash.error_description);
+        alert('There was an error: ' + hash.error + '\n' + hash.error_description + ' You may have clicked on the login email more than once. The login email link only works once. Please go directly to https://franciscan.university/bulletin . If you still are seeing this message, please contact Jesse Weigel (jweigel@franciscan.edu) and include the name of your browser and device in your email.');
         window.location.hash('l');
         window.location.hash('');
       }
